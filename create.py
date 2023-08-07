217bf7db4d35f87870e3fde5db29fd9b2002e844
Mitch L Newman <mitchellnewman3@gmail.com>
2023-08-07 16:29:35 +0100

    indented to correct code

--- create.py
+++ create.py
@@ -4,6 +4,6 @@ with app.app_context():
     db.drop_all()
     db.create_all()
 
-testuser = Person(first_name='Grooty',last_name='Toot') # Extra: this section populates the table with an example entry
-db.session.add(testuser)
-db.session.commit()
+    testuser = Person(first_name='Grooty',last_name='Toot') # Extra: this section populates the table with an example entry
+    db.session.add(testuser)
+    db.session.commit()
